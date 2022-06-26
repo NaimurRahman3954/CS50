48958d57386d42c8c1826f31eae43a15bdc7915e
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-06-26 11:37:33 +0600

    commit

--- Algorithms/bubbleSort.c
+++ Algorithms/bubbleSort.c
@@ -5,4 +5,23 @@ int main(void)
    // big O notation: O(n^2) in bubble sort
    // best case: O(n) in bubble sort
    int temp = 0;
+   int numbers[8] = {5, 4, 3, 2, 1, 0, 9, 8};
+
+   for (int i = 0; i < 8; i++)
+   {
+      for (int j = 0; j < 8 - i; j++)
+      {
+         if (numbers[j] > numbers[j + 1])
+         {
+            // swap the two elements
+            temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+         }
+      }
+   }
+   for (int i = 0; i < 8; i++)
+   {
+      printf("%d\n", numbers[i]);
+   }
 }
